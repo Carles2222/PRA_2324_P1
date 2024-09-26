f460aec0f2688906d77703c58fef1ed532f4846a
Carles Galdon <carlesgaldon@wifialu32-199.wi-fi.upv.es>
2024-09-26 17:27:23 +0200

    Añadida implementación de la clase ListArray

---
+++ ListArray.h
@@ -0,0 +1,14 @@
+#include <ostream>
+#include "List.h"
+
+template <typename T> 
+class ListArray : public List<T> {
+
+    private:
+        // miembros privados
+
+    public:
+        // miembros públicos, incluidos los heredados de List<T>
+    
+};
+
