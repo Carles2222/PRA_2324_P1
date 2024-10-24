e9488a879b0aa989522cd9c728613d521dce2ad4
Carles Galdon <carlesgaldon@wifialum33-63.wi-fi.upv.es>
2024-10-24 18:19:43 +0200

    4 commit

--- Node.h
+++ Node.h
@@ -1,9 +1,31 @@
-#include <ostream>
+#ifndef NODE_H
+#define NODE_H
 
-template <typename T> 
+#include <iostream>
+
+template <typename T>
 class Node {
-    public:
-        // miembros públicos
+public:
+    T data;      // Almacena el valor del nodo
+    Node* next;  // Puntero al siguiente nodo
+
+    // Constructor por defecto
+    Node() : next(nullptr) {}
+
+    // Constructor con un valor
+    Node(const T& data) : data(data), next(nullptr) {}
+
+    // Destructor
+    ~Node() {
+        next = nullptr;  // No es necesario liberar memoria manualmente
+    }
     
+    // Imprimir el nodo (sobrecarga del operador <<)
+    friend std::ostream& operator<<(std::ostream& os, const Node<T>& node) {
+        os << node.data;
+        return os;
+    }
 };
+
+#endif  // NODE_H
 
